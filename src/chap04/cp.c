3d1779748993ecdc52151222df73607f1a563378
sajadzirak <sajadzirakf@gmail.com>
2025-07-04 12:42:34 +0330

    implement cp.c for chap04

---
+++ src/chap04/cp.c
@@ -0,0 +1,71 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+void error_handler(const char *error_msg)
+{
+    perror(error_msg);
+    exit(EXIT_FAILURE);
+}
+
+void safe_write(int fd, const char *buf, size_t n)
+{
+    for (ssize_t total_written = 0, bytes_written; 
+            total_written < n;
+            total_written += bytes_written)
+    {
+        if ((bytes_written = write(fd, buf + total_written, n - total_written)) == -1)
+            error_handler("Writing to dst file");
+    }
+}
+
+#define BUF_SIZE 2048
+
+int main(int argc, char *argv[])
+{
+    if (argc != 3)
+    {
+        printf("Usage: cp [SRC] [DST]\n");
+        exit(EXIT_FAILURE);
+    }
+
+    int src_fd, dst_fd;
+    if ((src_fd = open(argv[1], O_RDONLY)) == -1)
+        error_handler("Opening src file");
+    if ((dst_fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 
+                        S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)) == -1)
+        error_handler("Opening dst file");
+
+    char buf[BUF_SIZE];
+    ssize_t bytes_read;
+    while ((bytes_read = read(src_fd, buf, BUF_SIZE)) > 0)
+    {
+        size_t i, j;
+        for (i = 0, j = 0; i < bytes_read; i++)
+        {
+            if (buf[i] == '\0')
+            {
+                if (i > j)
+                    safe_write(dst_fd, buf + j, i - j);
+                for (j = i + 1; j < bytes_read && buf[j] == '\0'; j++);
+                if (lseek(dst_fd, j - i, SEEK_CUR) == -1)
+                    error_handler("lseek");
+                i = j - 1;
+            }
+        }
+
+        if (j < bytes_read)
+            safe_write(dst_fd, buf + j, bytes_read - j);
+    }
+
+    if (bytes_read == -1)
+        error_handler("Reading src file");
+
+    if (close(src_fd) == -1)
+        error_handler("Closing src file");
+    if (close(dst_fd) == -1)
+        error_handler("Closing dst file");
+
+    return 0;
+}
