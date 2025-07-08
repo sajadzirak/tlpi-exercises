fee2a4fbf6de6e00c4ced954b3813e53cf0baaf5
sajadzirak <sajadzirakf@gmail.com>
2025-07-08 12:54:54 +0330

    practice02

---
+++ src/chap05/p02.c
@@ -0,0 +1,19 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include "tlpi_hdr.h"
+
+int main(int argc, char *argv[])
+{
+    int fd;
+    if ((fd = open("append_test.txt", O_APPEND | O_WRONLY)) == -1)
+        errExit("open");
+    
+    if (lseek(fd, 0, SEEK_SET) == -1)
+        errExit("lseek");
+
+    if (write(fd, "let's write some data.", 22) < 22)
+        errExit("write");
+
+    if (close(fd) == -1)
+        errExit("close");
+}
