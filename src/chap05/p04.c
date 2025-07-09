528a6674400627dd669ad3f7d038d18bfe448fbb
sajadzirak <sajadzirakf@gmail.com>
2025-07-09 15:22:20 +0330

    practice 5-4

---
+++ src/chap05/p04.c
@@ -0,0 +1,39 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
+#include "tlpi_hdr.h"
+
+int fcntl_dup(int oldfd)
+{
+    if (fcntl(oldfd, F_GETFL) < 0)
+    {
+        errno = EBADF;
+        return -1;
+    }
+    
+    int newfd = fcntl(oldfd, F_DUPFD, 0);
+    return newfd;
+}
+
+int fcntl_dup2(int oldfd, int newfd)
+{
+    if (fcntl(oldfd, F_GETFL) < 0)
+    {
+        errno = EBADF;
+        return -1;
+    }
+    
+    if (oldfd == newfd)
+        return newfd;
+
+    close(newfd);  // ignore close errors like what dup2 does
+
+    int fd;
+    if ((fd = fcntl(oldfd, F_DUPFD, newfd)) != newfd)
+    {
+        close(fd);
+        return -1;
+    }
+
+    return fd;
+}
